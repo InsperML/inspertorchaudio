cbd28db5c00288575a45aaf0ebc37ebbf9660acc
Fabio Ayres <fabioja@insper.edu.br>
2025-06-26 17:00:21 +0000

    chore: formatting

--- src/inspertorchaudio/data/dataloader.py
+++ src/inspertorchaudio/data/dataloader.py
@@ -8,18 +8,20 @@ from torch.utils.data import Dataset
 class AudioFileDataset(Dataset):
     def __init__(
         self,
-        # (audio_file_path, label_index)
         dataset_items: list[tuple[Path, int]],
         target_sample_rate: int,
-        # audio_file_path -> (audio_tensor, sample_rate)
         file_loader: Callable[[Path], tuple[torch.Tensor, int] | None],
-        # (audio_tensor, sample_rate, target_sample_rate) -> audio_tensor
         resampler: Callable[[torch.Tensor, int, int], torch.Tensor],
     ) -> None:
+        '''
+        dataset_items: (audio_file_path, label_index)
+        file_loader: audio_file_path -> (audio_tensor, sample_rate)
+        resampler: (audio_tensor, sample_rate, target_sample_rate) -> audio_tensor
+        '''
         self.dataset_items = dataset_items
+        self.target_sample_rate = target_sample_rate
         self.file_loader = file_loader
         self.resampler = resampler
-        self.target_sample_rate = target_sample_rate
 
     def __len__(self) -> int:
         return len(self.dataset_items)
