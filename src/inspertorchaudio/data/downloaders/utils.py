328221df1ec7e7cd6e9cb38bd13e50b15e286d30
Fabio Ayres <fabioja@insper.edu.br>
2025-07-31 18:49:04 +0000

    refactor: improve dataset download function and clean up code

--- src/inspertorchaudio/data/downloaders/utils.py
+++ src/inspertorchaudio/data/downloaders/utils.py
@@ -1,6 +1,6 @@
 import os
 import zipfile
-from importlib.resources import files
+import importlib.resources
 from pathlib import Path
 
 import dotenv
@@ -40,45 +40,49 @@ def download_file(target_url: str, target_path: str | Path) -> None:
             if chunk:
                 f.write(chunk)
                 pbar.update(len(chunk))
-                
+
+
 def unzip_file(zip_path: str | Path, extract_to: str | Path) -> None:
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
-        print(f"Extracted {zip_path} to {extract_to}")
+        print(f'Extracted {zip_path} to {extract_to}')
 
 
-def download_dataset(dataset_tag : str, force_download: bool = False) -> Path | None:
+def download_dataset(dataset_tag: str, force_download: bool = False) -> Path | None:
     """
     Downloads a dataset file.
     """
-    resource_path = files("inspertorchaudio.resources")
-    config_filename = Path("datasets.toml")
-    resource_path = resource_path / config_filename
 
-    with resource_path.open("r") as f:
-        data = toml.load(f)
-    
+    data = toml.loads(
+        importlib.resources.files('inspertorchaudio.resources')
+        .joinpath('datasets.toml')
+        .read_text()
+    )
+
     if dataset_tag not in data:
-        raise ValueError(f"Dataset {dataset_tag} not found in configuration file.")
-    
+        raise ValueError(f'Dataset {dataset_tag} not found in configuration file.')
+
     url = data[dataset_tag]['url']
-    filename = url.split("/")[-1]
-    
+    filename = url.split('/')[-1]
+
     dotenv.load_dotenv()
-    download_dir = Path(os.getenv("DATA_DIR")).expanduser()
+    data_dir_str = os.getenv('DATA_DIR')
+    if not data_dir_str:
+        raise RuntimeError('DATA_DIR environment variable is not set')
+
+    download_dir = Path(data_dir_str).expanduser()
     local_dir = data[dataset_tag]['local_dir']
-    
-    
     target_path = download_dir / local_dir / filename
 
     if target_path.exists() and not force_download:
-        print(f"File {target_path} already exists. Skipping download.")
+        print(f'File {target_path} already exists. Skipping download.')
         return target_path
 
     try:
         download_file(url, target_path)
         return target_path
     except Exception as e:
-        print(f"Failed to download at {url} or extract file {filename} for dataset {dataset_tag}: {e}")
+        print(
+            f'Failed to download at {url} or extract file {filename} for dataset {dataset_tag}: {e}'
+        )
         return None
-    
