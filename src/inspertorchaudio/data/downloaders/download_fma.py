4d810ae3a7236a5dc5aa622403d9378d400de9ef
Tiago Tavares <tiagoft@gmail.com>
2025-08-04 09:53:21 -0300

    chore: moved dataset config loading to utils.py

--- src/inspertorchaudio/data/downloaders/download_fma.py
+++ src/inspertorchaudio/data/downloaders/download_fma.py
@@ -1,12 +1,10 @@
-import importlib.resources
-import os
+
 from pathlib import Path
 
-import dotenv
 import pandas as pd
-import toml
 
-from .utils import download_dataset, unzip_file
+from .utils import (download_dataset, get_download_datadir, get_resources,
+                    unzip_file)
 
 
 def get_fma_metadata_index(force_download: bool = False) -> None:
@@ -14,17 +12,8 @@ def get_fma_metadata_index(force_download: bool = False) -> None:
     Downloads the FMA metadata zip file and extracts it to the specified directory.
     Returns a path to tracks.csv, which is the index file for the FMA dataset.
     """
-    data = toml.loads(
-        importlib.resources.files('inspertorchaudio.resources')
-        .joinpath('datasets.toml')
-        .read_text()
-    )
-
-    dotenv.load_dotenv()
-    data_dir_str = os.getenv('DATA_DIR')
-    if not data_dir_str:
-        raise RuntimeError('DATA_DIR environment variable is not set')
-    download_dir = Path(data_dir_str).expanduser()
+    download_dir = get_download_datadir()
+    data = get_resources()
     local_dir = data['fma_metadata']['local_dir']
     target_file = download_dir / local_dir / 'fma_metadata' / 'tracks.csv'
 
